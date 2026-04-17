266ea66444248c2c4b01f23a78fe813888931db3
Ubuntu <ubuntu@ip-172-31-19-4.ec2.internal>
2026-04-17 10:08:33 +0000

    files added in stg branch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-<<<<<<< HEAD
-    port: 5175
-=======
     port: 5173
->>>>>>> 5409204 (files added in dev brach)
   }
 })
