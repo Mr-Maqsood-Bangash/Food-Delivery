fea0bcaf0e3edf8aef9e28ade78a6cf60bee1aa4
Ubuntu <ubuntu@ip-172-31-19-4.ec2.internal>
2026-04-17 06:31:09 +0000

    testing done in feature barach

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -30,7 +30,7 @@ const Cart = () => {
         <br />
         <hr />
         {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0) {
+          if (cartItems?.[item._id] > 0) {
             return (
               <div>
                 <div className="cart-items-title cart-items-item">
